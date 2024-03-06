395d38ca33a2dabe37a8b603fcb8f22dc46ea85f
Guilherme Sandi <guilherme_sandi@hotmail.com>
2024-03-06 19:55:22 -0300

    Add new components and styles

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,47 +1,67 @@
-import { ScrollView, Text, View } from "react-native";
+import { Alert, ScrollView, Text, View } from "react-native";
 
 import { styles } from "./styles";
 import { Ingredient } from "@/components/Ingredient";
 import { useState } from "react";
+import { Selected } from "@/components/Selected";
 
 export default function Index() {
   const [selected, setSelected] = useState<string[]>([]);
 
   function handleToggleSelected(value: string) {
     if (selected.includes(value)) {
-      return setSelected((state) => state.filter((item) => item !== value))
+      return setSelected((state) => state.filter((item) => item !== value));
     }
 
-    setSelected((state) => [...state, value])
+    setSelected((state) => [...state, value]);
+  }
+
+  function handleClearSelected() {
+    Alert.alert("Limpar", "Deseja limpar tudo?", [
+      {
+        text: "Não",
+        style: "cancel",
+      },
+      {
+        text: "Sim",
+        onPress: () => setSelected([]),
+      },
+    ]);
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        Escolha {'\n'}
+        Escolha {"\n"}
         <Text style={styles.subtitle}>os produtos</Text>
       </Text>
 
       <Text style={styles.message}>
         Descubra receitas baseadas nos produtos que você escolheu.
       </Text>
-      
+
       <ScrollView
         contentContainerStyle={styles.ingredients}
         showsVerticalScrollIndicator={false}
       >
-        {
-          Array.from({ length: 100 }).map((_, index) => (
-            <Ingredient
-              key={index}
-              name="Tomate"
-              image=""
-              selected={selected.includes(String(index))}
-              onPress={() => handleToggleSelected(String(index))}
-            />
-          ))
-        }
+        {Array.from({ length: 100 }).map((_, index) => (
+          <Ingredient
+            key={index}
+            name="Tomate"
+            image=""
+            selected={selected.includes(String(index))}
+            onPress={() => handleToggleSelected(String(index))}
+          />
+        ))}
       </ScrollView>
+
+      {selected.length > 0 && (
+        <Selected
+          quantity={selected.length}
+          onClear={handleClearSelected}
+          onSearch={() => {}}
+        />
+      )}
     </View>
-  )
+  );
 }
